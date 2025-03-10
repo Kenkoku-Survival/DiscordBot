f208c2669fb6de14eb4e45df8d2b2b6bcf1ed7e0
pothca <1016009seiya@gmail.com>
2025-03-10 23:42:55 +0900

    コミットできていないファイルが多かったので一気に追加

---
+++ events/agreeDiscordServerRules.js
@@ -0,0 +1,23 @@
+const config = require('../config.js');
+
+module.exports = async (client, reaction, user, isAdding) => {
+    try {
+      if (reaction.message.id !== config.AGREE_DISCORD_SERVER_RULE_MESSAGE || user.bot || reaction.emoji.name !== "✅") return;
+  
+      const guild = await client.guilds.fetch(config.GUILD_ID);
+      const member = await guild.members.fetch(user.id);
+      const role = await guild.roles.fetch(config.AGREE_DISCORD_SERVER_RULE_ROLE);
+  
+      if (!role || !member) return;
+  
+      if (isAdding) {
+        await member.roles.add(role);
+        console.log(`✅ Added 'agree discord server rules role' to ${user.tag}`);
+      } else {
+        await member.roles.remove(role);
+        console.log(`🚫 Removed 'agree discord server rules role' from ${user.tag}`);
+      }
+    } catch (error) {
+      console.error("❌ Error handling reaction for 'agree discord server rules':", error);
+    }
+  };
---
+++ index.js
@@ -0,0 +1,42 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const config = require("./config.js");
+const agreeDiscordServerRule = require('./events/agreeDiscordServerRules.js');
+const agreeMinecraftServerRule = require('./events/agreeMinecraftServerRule.js');
+
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.GuildMembers,
+    ],
+  });
+
+  client.once('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}`);
+  
+    try {
+      // サーバー情報を取得する前にボットが準備できていることを確認
+      const guild = await client.guilds.fetch(config.GUILD_ID);
+      const discord_rule_message = await guild.channels.cache.get(config.AGREE_DISCORD_SERVER_RULE_CHANNEL).messages.fetch(config.AGREE_DISCORD_SERVER_RULE_MESSAGE);
+      const minecraft_rule_message = await guild.channels.cache.get(config.AGREE_MINECRAFT_SERVER_RULE_CHANNEL).messages.fetch(config.AGREE_MINECRAFT_SERVER_RULE_MESSAGE);
+  
+      // メッセージにリアクションを追加
+      await discord_rule_message.react('✅');
+      console.log('Reaction added to the server rules message.');
+      await minecraft_rule_message.react('✅');
+      console.log('Reaction added to minecraft rules message.');
+  
+    } catch (error) {
+      console.error('Error while adding reaction:', error);
+    }
+  });
+
+client.on(Events.MessageReactionAdd, (reaction, user) => agreeDiscordServerRule(client, reaction, user, true));
+client.on(Events.MessageReactionRemove, (reaction, user) => agreeDiscordServerRule(client, reaction, user, false));
+client.on(Events.MessageReactionAdd, (reaction, user) => agreeMinecraftServerRule(client, reaction, user, true));
+client.on(Events.MessageReactionRemove, (reaction, user) => agreeMinecraftServerRule(client, reaction, user, false));
+
+
+client.login(config.TOKEN);
